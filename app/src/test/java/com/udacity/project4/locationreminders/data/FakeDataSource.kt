cc49ab5e78d0548c88d5ef055611cb91e7419c78
arwakamal <arwakamal336@gmail.com>
2022-11-18 01:11:11 +0200

    "Explanation what I did on FakeData Source"

--- app/src/test/java/com/udacity/project4/locationreminders/data/FakeDataSource.kt
+++ app/src/test/java/com/udacity/project4/locationreminders/data/FakeDataSource.kt
@@ -3,26 +3,65 @@ package com.udacity.project4.locationreminders.data
 import com.udacity.project4.locationreminders.data.dto.ReminderDTO
 import com.udacity.project4.locationreminders.data.dto.Result
 
-//Use FakeDataSource that acts as a test double to the LocalDataSource
-class FakeDataSource : ReminderDataSource {
 
-//    TODO: Create a fake data source to act as a double to the real data source
+//Use FakeDataSource that acts as a test double to the LocalDataSource
+class FakeDataSource(var reminders: MutableList<ReminderDTO> = mutableListOf()) : ReminderDataSource {
+
+    private var shouldReturnError = false
+
+    fun setReturnError(value: Boolean) {
+        shouldReturnError = value
+    }
+
 
     override suspend fun getReminders(): Result<List<ReminderDTO>> {
-        TODO("Return the reminders")
+        // get all reminders
+
+        // if we need to test it with error handling.
+        // i make data source return error even if not empty to test error.
+        // for the network I talk generally and our app not contain any network.
+        // the error could be something with the db
+
+
+        if (shouldReturnError) {
+            return Result.Error("Returning testing error!")
+        }
+
+        //as you order. when no reminders are found,
+        // Room simply returns an empty list and the data source actually returns Result.Success
+        if (reminders.isEmpty()) {
+            return Result.Success(reminders)
+        } else {
+            return Result.Success(reminders)
+        }
     }
 
     override suspend fun saveReminder(reminder: ReminderDTO) {
-        TODO("save the reminder")
+        // save reminders
+        reminders.add(reminder)
     }
 
     override suspend fun getReminder(id: String): Result<ReminderDTO> {
-        TODO("return the reminder with the id")
+        //get reminder with id
+
+        if(shouldReturnError) {
+            return Result.Error("Returning testing error!")
+        }
+
+        // if reminder found with this id
+        val result = reminders.firstOrNull{it.id == id}
+        result?.let {
+            return Result.Success(it)
+        }
+
+        // if reminder not found with this id
+        return Result.Error("This Reminder not available")
+
     }
 
     override suspend fun deleteAllReminders() {
-        TODO("delete all the reminders")
+        //delete all reminders
+        reminders.clear()
     }
-
 
 }
